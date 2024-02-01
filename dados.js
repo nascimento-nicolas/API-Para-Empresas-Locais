8b199ffdfc92ea8de7b7c0ada0714ef32395d672
Nicolas Nascimento <135062144+nascimento-nicolas@users.noreply.github.com>
2024-01-31 22:36:59 -0300

    Atualizando Dados

--- dados.js
+++ dados.js
@@ -1,18 +1,17 @@
 
 const empresas=[
-    {nome:"Mercadinho Da Villa",descrição:"descrição", endereço:" ", ceo:" ", objetivo: "objetivo", faturamentoAnual:" ", metas: " "},
-
-    {nome:"Lanchonete 200",descrição:"descrição", endereço:" ", ceo:" ", objetivo: "objetivo", faturamentoAnual:" ", metas: " "}
+    {nome:"MercadoLivre",descrição:"Empresa grande com objetivo de ajudar empresas menores a crescer", endereço:"R.Gomes de Carvalho 1510 ", ceo:"Stelleo Tolda ", objetivo: "democratizar o comércio eletrônico oferecendo a melhor plataforma e os serviços necessários para que pessoas e empresas possam comprar, pagar, vender, enviar, anunciar e gerir seus negócios na Internet.", faturamentoAnual:"US$ 2,1 bilhões", metas: " manter e ampliar a liderança em e-commerce e entre as fintechs no Brasil”, diz. “A logística é a face mais visível, mas estamos fazendo muito mais."} 
 
 ]
 
 const empresaspequenas =[
-    {nome:"Mercadinho Da Villa",descrição:"descrição", endereço:" "}
+    {nome:"Barbosa SuperMercados", descrição:"Tudo começou em 1976, quando o Sr. Antônio e a Dona Hilda decidiram montar o próprio negócio, no bairro de Taboão em Guarulhos. Com o passar do tempo e o crescimento do bairro, o pequeno negócio da família, se transformou uma das principais referências em mercearia na região.", endereço:"Av.João de Andrade, 1500", ceo:"Juliano Kerth Eckhard", objetivo:"Ser uma empresa ética, aperfeiçoando todos os processos e buscando possuir o melhor, transferindo ao consumidor todos os benefícios alcançados, garantindo assim a sua preferência e o crescimento constante da empresa.", faturamentoAnual:" R$ 9,4 bilhões", metas:" a meta de chegar a 45 unidades até 2025"}
 ]
 
 const sites = [
     {nome:"conta azul", descrição:"Site sobre dicas para gerir um negócio", link: "https://blog.contaazul.com/como-administrar-uma-empresa/" },
-    {nome:"", descrição: " ", link:" "}
+    {nome:"agendor", descrição: "site sobre como crescer uma empresa ", link:"https://www.agendor.com.br/blog/estrategia-de-crescimento-de-uma-empresa/ "},
+    {nome:"convenia", descrição: "Site de como ter um bom-estar no trabalho ", link:"https://blog.convenia.com.br/bem-estar-no-trabalho/ "},
 ]
 
 export{empresas, empresaspequenas, sites}
